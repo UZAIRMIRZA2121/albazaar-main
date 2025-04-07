b77d24c18f950c15be1ba66f4b5b1b44cad70d40
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-04-07 06:29:11 +0500

    fix: remove debug dump and commented call_user_func in PaytabsController payment method

--- app/Http/Controllers/Payment_Methods/PaytabsController.php
+++ app/Http/Controllers/Payment_Methods/PaytabsController.php
@@ -178,13 +178,13 @@ class PaytabsController extends Controller
             ]);
          
             $payment_data = $this->payment::where(['id' => $request['payment_id']])->first();
-            dd($payment_data);
+        
             if (isset($payment_data) && function_exists($payment_data->success_hook)) {
           
-                call_user_func($payment_data->success_hook, $payment_data);
+                // call_user_func($payment_data->success_hook, $payment_data);
             }
 
-           
+            dd($payment_data);
             return $this->payment_response($payment_data,'success');
         }
         $payment_data = $this->payment::where(['id' => $request['payment_id']])->first();
