56e3699a63b53691ce2cd9e7fee955f30dc81799
Uzair mirza <UZAIRMIRZA2121@GMAIL.COM>
2025-04-07 08:39:37 +0500

    fix: update NotifySellerMail to include seller's email in constructor and set as recipient

--- app/Mail/NotifySellerMail.php
+++ app/Mail/NotifySellerMail.php
@@ -10,15 +10,18 @@ class NotifySellerMail extends Mailable
     use Queueable, SerializesModels;
 
     public $orderDetails; // Declare as a public property
+    public $sellerEmail; // Declare seller's email as a public property
 
     /**
      * Create a new message instance.
      *
      * @param $orderDetails
+     * @param $sellerEmail
      */
-    public function __construct($orderDetails)
+    public function __construct($orderDetails, $sellerEmail)
     {
         $this->orderDetails = $orderDetails; // Set the orderDetails
+        $this->sellerEmail = $sellerEmail;   // Set the seller's email
     }
 
     /**
@@ -28,9 +31,9 @@ class NotifySellerMail extends Mailable
      */
     public function build()
     {
-        // You can add additional info here if needed, like the seller's email.
         return $this->subject('You have new orders!')
-                ->view('email-templates.notify-seller')
+                    ->to($this->sellerEmail) // Set the recipient's email
+                    ->view('email-templates.notify-seller')
                     ->with(['orderDetails' => $this->orderDetails]); // Pass orderDetails to the view
     }
 }
